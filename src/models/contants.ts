5cc8e8d61a88117d0df70e9c39f9a1e5b50e94fc
Kevin SG <kevin.boster99@gmail.com>
2022-06-20 16:00:10 -0300

    Deleted obsolete files

--- src/models/contants.ts
+++
@@ -1,5 +0,0 @@
-// Type paths route
-export type TPathsRoute = {
-  user: "/api/user";
-  login: "/api/login";
-};
--- src/models/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as Server } from "./server";
-export { default as User } from "./user";
-export * from "./contants";
