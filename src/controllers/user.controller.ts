1ebc9a4e3521aae9ddbae5942319eedbf7d0e799
Kevin SG <kevin.boster99@gmail.com>
2022-06-20 14:45:55 -0300

    Controller updated

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,44 +1,55 @@
-import { Request, Response } from "express";
+import type { Request, Response } from "express";
 
-import { successHandler, errorHandler } from "@/utilities";
+import * as Global from "@/global";
+import * as Utility from "@/utilities";
+import * as UseCase from "@/use_cases";
 
 class UserController {
   public async getUser(req: Request, res: Response): Promise<void> {
     try {
       // Dome some with success here
-      res.status(200).json(successHandler("OK", { data: "GET" }, res.statusCode));
+      res.status(200).json(Utility.successHandler("OK", { data: "GET" }, res.statusCode));
     } catch (err) {
-      res.status(401).json(errorHandler("Not authorized", res.statusCode));
+      res.status(401).json(Utility.errorHandler("Unauthorized", res.statusCode));
     }
   }
 
-  public async postUser({ body }: Request, res: Response): Promise<void> {
+  public async postUser(req: Request, res: Response): Promise<void> {
+    const { name, email, password } = req.body;
     try {
-      // Dome some with success here
-      res.status(201).json(successHandler("OK", { data: body }, res.statusCode));
+      const { user, token } = await UseCase.createUser({ name, email, password });
+
+      res
+        .status(Global.EnumHttpStatusCodes.CREATE)
+        .cookie(Global.environmentVariables.COOKIE_V1_KEY, token, {
+          path: Global.EnumPathApiVersion.V1,
+          httpOnly: true,
+        })
+        .json(Utility.successHandler(Global.EnumHttpStatusMessages.OK, { data: user }, res.statusCode));
     } catch (err) {
-      // Do some with error here
-      res.status(401).json(errorHandler("Not authorized", res.statusCode));
+      res
+        .status(Global.EnumHttpStatusCodes.UNAUTHOTIZED)
+        .json(Utility.errorHandler(Global.EnumHttpStatusMessages.UNAUTHOTIZED, res.statusCode));
     }
   }
 
   public async updateUser(req: Request, res: Response): Promise<void> {
     try {
       // Dome some with success here
-      res.status(201).json(successHandler("OK", { data: "PUT" }, res.statusCode));
+      res.status(201).json(Utility.successHandler("OK", { data: "PUT" }, res.statusCode));
     } catch (err) {
       // Do some with error here
-      res.status(401).json(errorHandler("Not authorized", res.statusCode));
+      res.status(401).json(Utility.errorHandler("Unauthorized", res.statusCode));
     }
   }
 
   public async deleteUser(req: Request, res: Response): Promise<void> {
     try {
       // Dome some with success here
-      res.status(201).json(successHandler("OK", { data: "DELETE" }, res.statusCode));
+      res.status(201).json(Utility.successHandler("OK", { data: "DELETE" }, res.statusCode));
     } catch (err) {
       // Do some with error here
-      res.status(401).json(errorHandler("Not authorized", res.statusCode));
+      res.status(401).json(Utility.errorHandler("Unauthorized", res.statusCode));
     }
   }
 }
