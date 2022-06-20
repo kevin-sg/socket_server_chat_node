cc90e6c61e446757e1eef8d2ed624aa7308e6540
Kevin SG <kevin.boster99@gmail.com>
2022-06-20 14:47:53 -0300

    Environments updated

--- src/global/environments.ts
+++ src/global/environments.ts
@@ -1,11 +1,10 @@
-interface EnvironmentVariables {
-  HOST_PORT: number;
-  HOST_CLIENT: string;
-  MONGODB_URI: string;
-}
+import * as Global from "@/global";
 
-export const environmentVariables: EnvironmentVariables = {
+export const environmentVariables: Global.IEnvironmentVariables = {
   HOST_PORT: parseInt(process.env.PORT as string),
   HOST_CLIENT: process.env.HOST_CLIENT as string,
   MONGODB_URI: process.env.MONGODB_URI as string,
+  COOKIE_V1_KEY: process.env.COOKIE_V1_KEY as string,
+  TOKEN_EXPIRATION_TIME: process.env.TOKEN_EXPIRATION_TIME as string,
+  SECRET_TOKEN_PRIVATE_KEY: process.env.SECRET_TOKEN_PRIVATE_KEY as string,
 };
